eb5f706ab25675e8c2bc4d194db4e5542cd181f6
Paulet Beltran <124410482+PauletBeltran@users.noreply.github.com>
2023-05-17 20:11:29 -0600

    base 18/05/23

--- Parcial2/spotifypirata.sql
+++ Parcial2/spotifypirata.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-04-2023 a las 03:08:45
+-- Tiempo de generación: 18-05-2023 a las 08:11:03
 -- Versión del servidor: 10.1.39-MariaDB
 -- Versión de PHP: 7.3.5
 
